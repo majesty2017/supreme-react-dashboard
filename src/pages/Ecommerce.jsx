fd33513d945c01f2a9bf32a92193884fc6e3c6e0
majesty2017 <reborndzre@gmail.com>
2022-12-30 03:17:20 +0000

    Initial Commit

---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ecommerce = () => {
+    return (
+        <div>
+            ECommerce
+        </div>
+    );
+};
+
+export default Ecommerce;
---
+++ src/pages/Editors.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Editors = () => {
+    return (
+        <div>
+            Editors
+        </div>
+    );
+};
+
+export default Editors;
